c0a4a9d28e585125bc2564fdbadfd629f19b651f
Hikmalaf <147985507+Hikmalaf@users.noreply.github.com>
2024-11-20 21:36:26 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";  // Import the path module
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [
+      {
+        find: "@", 
+        replacement: path.resolve(__dirname, "src")  // Use path.resolve for absolute path
+      }
+    ],
   },
 });
