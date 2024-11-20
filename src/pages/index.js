a4173e315983906246a2f91f936f3703a35b933e
Hikmalaf <147985507+Hikmalaf@users.noreply.github.com>
2024-11-20 23:09:55 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 // src/pages/index.js
-export { default as Home } from "./home";
-export { default as Calculator } from "./calculator";
-export { default as TentangCarbon } from "./tentang-carbon";
-export { default as Peta } from "./peta";
-export { default as Donasi } from "./donasi";
-export { default as AboutMangrovePage } from "./mangrove";
+export { default as Home } from "./pages/home";
+export { default as Calculator } from "./pages/calculator";
+export { default as TentangCarbon } from "./pages/tentang-carbon";
+export { default as Peta } from "./pages/peta";
+export { default as Donasi } from "./pages/donasi";
+export { default as AboutMangrovePage } from "./pages/mangrove";
