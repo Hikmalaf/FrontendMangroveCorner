918e522cccf8648b59b95cd20252084ee1bd0b99
Hikmalaf <147985507+Hikmalaf@users.noreply.github.com>
2024-11-20 23:13:46 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';  // Impor modul path
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-  alias: {
-      '@': path.resolve(__dirname, 'src'),  // Menetapkan alias '@' untuk folder 'src'
-    }
-
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Menetapkan alias '@' untuk folder 'src'
     }
   }
 });
